0be75a2e473ef9108052a00fa3df4683f8dbfb4f
yyxxryrx <yyxxryrx@aliyun.com>
2026-08-25 18:53:53 +0800

    feat(player): add audio decoding and playback

--- src-c/lib.c
+++ src-c/lib.c
@@ -1,12 +1,15 @@
 #include "libavcodec/codec_par.h"
+#include "libavutil/channel_layout.h"
 #include "libavutil/frame.h"
 #include "libavutil/pixfmt.h"
+#include "libavutil/samplefmt.h"
 #include <libavcodec/avcodec.h>
 #include <libavcodec/codec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/avutil.h>
 #include <libavutil/error.h>
 #include <libavutil/rational.h>
+#include <libswresample/swresample.h>
 #include <libswscale/swscale.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -31,6 +34,36 @@ int32_t find_best_video(AVFormatContext *ctx) {
 
 int32_t find_best_audio(AVFormatContext *ctx) {
     return av_find_best_stream(ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+}
+
+int32_t new_codec_ctx(AVFormatContext *ctx, int32_t index,
+                      AVCodecContext *codec_ctx) {
+    if (ctx == NULL || codec_ctx == NULL || index < 0 ||
+        index >= ctx->nb_streams)
+        return -1;
+
+    AVStream *stream = ctx->streams[index];
+    if (stream == NULL)
+        return -1;
+
+    const AVCodecParameters *codec_params = stream->codecpar;
+
+    const AVCodec *codec = avcodec_find_decoder(codec_params->codec_id);
+    if (codec == NULL)
+        return -1;
+
+    int ret = avcodec_parameters_to_context(codec_ctx, codec_params);
+    if (ret != 0) {
+        avcodec_free_context(&codec_ctx);
+        return ret;
+    }
+
+    ret = avcodec_open2(codec_ctx, codec, NULL);
+    if (ret != 0) {
+        avcodec_free_context(&codec_ctx);
+        return ret;
+    }
+    return 0;
 }
 
 bool get_info(AVFormatContext *ctx, int32_t index, InfoResult *res) {
@@ -102,3 +135,62 @@ int scale(SwsContext *ctx, AVFrame *src, AVFrame *dst) {
 uint8_t **get_data(AVFrame *frame) { return frame->data; }
 
 int32_t *get_linesize(AVFrame *frame) { return frame->linesize; }
+
+int32_t new_swr(SwrContext **swr, AVCodecContext* const ctx,
+                int32_t out_sample_rate) {
+    AVChannelLayout layout = AV_CHANNEL_LAYOUT_STEREO;
+    int ret = swr_alloc_set_opts2(swr, &layout, AV_SAMPLE_FMT_S16,
+                                  out_sample_rate, &ctx->ch_layout, ctx->sample_fmt,
+                                  ctx->sample_rate, 0, NULL);
+    if (ret < 0)
+        return ret;
+
+    ret = swr_init(*swr);
+    if (ret < 0) {
+        swr_free(swr);
+        return ret;
+    }
+    return 0;
+}
+
+int32_t resample_frame(SwrContext *swr, const AVFrame *in_frame,
+                       AVFrame *out_frame, int out_sample_rate) {
+    if (in_frame == NULL || out_frame == NULL)
+        return 0;
+
+    int out_nb_samples = swr_get_out_samples(swr, in_frame->nb_samples);
+    if (out_nb_samples <= 0)
+        return 0;
+
+    
+    out_frame->format = AV_SAMPLE_FMT_S16;
+    out_frame->ch_layout = (AVChannelLayout)AV_CHANNEL_LAYOUT_STEREO;
+    out_frame->sample_rate = out_sample_rate;
+    out_frame->nb_samples = in_frame->nb_samples;
+
+    int ret = av_frame_get_buffer(out_frame, 0);
+    if (ret < 0)
+        return ret;
+
+    ret = swr_convert(swr, out_frame->data, out_nb_samples,
+                      (const uint8_t **)in_frame->data, in_frame->nb_samples);
+    if (ret < 0) {
+        av_frame_unref(out_frame);
+        return ret;
+    }
+
+    out_frame->nb_samples = ret;
+    out_frame->sample_rate = out_sample_rate;
+
+    // ret = av_channel_layout_copy(&out_frame->ch_layout, &in_frame->ch_layout);
+    // if (ret < 0) {
+    //     av_frame_unref(out_frame);
+    //     return ret;
+    // }
+
+    return out_frame->nb_samples;
+}
+
+int32_t get_size(AVFrame* frame) {
+    return frame->nb_samples * 2 * av_get_bytes_per_sample(frame->format);
+}
